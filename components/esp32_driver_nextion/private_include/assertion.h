80b381b56fbbccc8d21814e5a131d78e26cbe93d
gfurtadoalmeida <27151609+gfurtadoalmeida@users.noreply.github.com>
2023-04-21 10:15:16 -0300

    Better documentation

--- components/esp32_driver_nextion/private_include/assertion.h
+++ components/esp32_driver_nextion/private_include/assertion.h
@@ -13,9 +13,8 @@ extern "C"
  * @param condition Condition to evaluate.
  * @param message Message to be logged if the condition is false.
  * @param return_value Value to be returned if the condition is false.
- * @return If the condition is false the method will return what was
- * passed on @ref return_value parameter, otherwise the method will
- * continue.
+ * @return \p return_value if the condition is false, otherwise the
+ * method will continue.
  */
 #define CMP_CHECK(condition, message, return_value)              \
     if (!(condition))                                            \
@@ -28,8 +27,8 @@ extern "C"
  * @brief Check if a Nextion handle is valid.
  * @param handle Nextion handle.
  * @param return_value Value to be returned if the condition fails.
- * @return If the condition fails the method will return what was passed on
- * "return_value" parameter, otherwise the method will continue.
+ * @return \p return_value if the condition is false, otherwise the
+ * method will continue.
  */
 #define CMP_CHECK_HANDLE(handle, return_value) CMP_CHECK((handle != NULL), "handle error(NULL)", return_value)
 
--- components/esp32_driver_nextion/private_include/log.h
+++ components/esp32_driver_nextion/private_include/log.h
@@ -8,12 +8,38 @@ extern "C"
 {
 #endif
 
+    /** @brief Description text used for logging. */
     static const char *TAG = "NEXTION";
 
-#define CMP_LOGD(format, ...) ESP_LOGD(TAG, format, ##__VA_ARGS__)
+/**
+ * @brief Log a verbose message.
+ * @param format String format.
+ * @param ... Arguments used by the string format.
+ */
 #define CMP_LOGV(format, ...) ESP_LOGV(TAG, format, ##__VA_ARGS__)
+/**
+ * @brief Log a debug message.
+ * @param format String format.
+ * @param ... Arguments used by the string format.
+ */
+#define CMP_LOGD(format, ...) ESP_LOGD(TAG, format, ##__VA_ARGS__)
+/**
+ * @brief Log an informational message.
+ * @param format String format.
+ * @param ... Arguments used by the string format.
+ */
 #define CMP_LOGI(format, ...) ESP_LOGI(TAG, format, ##__VA_ARGS__)
+/**
+ * @brief Log a warning message.
+ * @param format String format.
+ * @param ... Arguments used by the string format.
+ */
 #define CMP_LOGW(format, ...) ESP_LOGW(TAG, format, ##__VA_ARGS__)
+/**
+ * @brief Log an error message.
+ * @param format String format.
+ * @param ... Arguments used by the string format.
+ */
 #define CMP_LOGE(format, ...) ESP_LOGE(TAG, format, ##__VA_ARGS__)
 
 #ifdef __cplusplus
