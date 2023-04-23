07a5f459021663210132eaf9a270c6bc4654b9f2
Gabriel Almeida <27151609+gfurtadoalmeida@users.noreply.github.com>
2023-04-22 22:45:42 -0300

    Added log for buffer and hex

--- components/esp32_driver_nextion/private_include/log.h
+++ components/esp32_driver_nextion/private_include/log.h
@@ -41,6 +41,22 @@ extern "C"
  * @param ... Arguments used by the string format.
  */
 #define CMP_LOGE(format, ...) ESP_LOGE(TAG, format, ##__VA_ARGS__)
+/**
+ * @brief Log a buffer of characters, separated into 16 bytes each line,
+ * at info level.
+ * @note Buffer should contain only printable characters.
+ * @param tag Description tag.
+ * @param buffer Buffer array.
+ * @param length Length of buffer in bytes.
+ */
+#define CMP_LOG_BUFFER_CHAR(tag, buffer, length) ESP_LOG_BUFFER_CHAR_LEVEL(tag, buffer, length, ESP_LOG_INFO)
+/**
+ * @brief Log a buffer of hex bytes at info level.
+ * @param tag Description tag.
+ * @param buffer Buffer array.
+ * @param length Length of buffer in bytes.
+ */
+#define CMP_LOG_BUFFER_HEX(tag, buffer, length) ESP_LOG_BUFFER_HEXDUMP(tag, buffer, length, ESP_LOG_INFO)
 
 #ifdef __cplusplus
 }
